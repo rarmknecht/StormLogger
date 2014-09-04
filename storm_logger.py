efe3361baaa9f325c1b68dd24f004a05b644dd5b
rarmknecht <rarmknecht@gmail.com>
2014-09-03 20:17:57 -0500

    PEP8 Compliance

--- storm_logger.py
+++ storm_logger.py
@@ -3,8 +3,9 @@ import urllib.request
 import socket
 import logging
 
+
 class StormLogger(logging.Handler):
-    
+
     INPUT_URL_FMT = 'https://%s/1/inputs/http'
 
     def __init__(self, access_token=None, project_id=None, api_fqdn=None):
@@ -16,35 +17,37 @@ class StormLogger(logging.Handler):
         self.url = self.INPUT_URL_FMT % self.api_fqdn
         self.pass_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
         self.pass_manager.add_password(None, self.url, 'x', access_token)
-        self.auth_handler = urllib.request.HTTPBasicAuthHandler(self.pass_manager)
+        self.auth_handler = urllib.request.HTTPBasicAuthHandler(
+            self.pass_manager
+        )
         self.opener = urllib.request.build_opener(self.auth_handler)
         urllib.request.install_opener(self.opener)
-                
+
     def usesTime(self):
         return False
 
     def emit(self, record):
-        
+
         try:
             response = self._send_to_splunk(record)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
-            # All errors end here.
             self.handleError(record)
 
     def _send_to_splunk(self, record):
         sourcetype = 'syslog'
         host = socket.gethostname()
-        
-        event_text = {'level' : record.levelname, 
-                      'module' : record.module,
-                      'line' : record.lineno,
-                      'data' : self.format(record)
-                    }
+
+        event_text = {'level': record.levelname,
+                      'module': record.module,
+                      'line': record.lineno,
+                      'data': self.format(record)
+                      }
         params = {'project': self.project_id,
                   'sourcetype': sourcetype,
-                  'host': host}
+                  'host': host
+                  }
 
         url = '%s?%s' % (self.url, urllib.parse.urlencode(params))
         try:
